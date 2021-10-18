bec909b7fe98a2201cf496d3ad433feba5d6fcb3
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-10-18 02:15:22 -0500

    js functionality

--- app.js
+++ app.js
@@ -20,12 +20,38 @@ let prole = document.getElementById("role")
 let pquote = document.getElementById("quote")
 let pimg = document.getElementById("pic")
 let toggle = document.getElementById("toggle")
+let control=document.getElementById("control")
+// let nextbtn=window.getComputedStyle(control,":before");
+// let prevbtn=window.getComputedStyle(control,":after");
 let next = false
-document.onload = fill();
 
-function fill() {
+
+document.onload = fillTanya();
+
+function fillTanya() {
     pname.innerHTML = tanya.name;
     prole.innerHTML = tanya.role;
     pquote.innerHTML = tanya.quote
     pimg.style.backgroundImage = `url("images/image-tanya.jpg")`
 }
+
+function fillJohn(){
+    pname.innerHTML = john.name;
+    prole.innerHTML = john.role;
+    pquote.innerHTML = john.quote
+    pimg.style.backgroundImage = `url("images/image-john.jpg")`
+}
+
+control.addEventListener("click",(e)=>{
+   let clickTarget=e.target;
+   const clickTargetWidth = clickTarget.offsetWidth;
+    const xCoordInClickTarget = e.clientX - clickTarget.getBoundingClientRect().left;
+    if (clickTargetWidth / 2 > xCoordInClickTarget) {
+    fillTanya()
+    } else {
+     fillJohn()
+    }
+
+})
+
+
